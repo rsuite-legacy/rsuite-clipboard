89a868d6db16b139bec63696bfd646aa76583a76
Godfey <hiyangguo@qq.com>
2017-11-20 18:14:37 +0800

    update:Optimize the code

--- src/component/Clipboard.js
+++ src/component/Clipboard.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { copy, select } from '../util';
+import _omit from 'lodash.omit';
+
+const propTypes = {
+    text: PropTypes.string.isRequired,
+    children: PropTypes.element.isRequired,
+    onCopy: PropTypes.func
+};
 
 class Clipboard extends Component {
-    static propTypes = {
-        text: PropTypes.string.isRequired,
-        children: PropTypes.element.isRequired,
-        onCopy: PropTypes.func
-    };
     static copy = copy;
     static select = select;
 
     constructor(props) {
         super(props);
-        this.onClick = this.onClick.bind(this);
     }
 
-    handleCopy(text, prevDom) {
+    handleCopy = (text, prevDom) => {
         return copy(text, prevDom);
     }
 
-    onClick(event) {
+    handleClick = (event) => {
         const {
             text,
             onCopy,
@@ -35,19 +36,15 @@ class Clipboard extends Component {
     }
 
     render() {
-        const {
-            text,
-            onCopy,
-            children,
-            ...props
-        } = this.props;
-        const elem = React.Children.only(children);
-
+        const elem = React.Children.only(this.props.children);
+        const elementProps = _omit(this.props, Object.keys(propTypes));
         return React.cloneElement(elem, {
-            ...props,
-            onClick: this.onClick
+            ...elementProps,
+            onClick: this.handleClick
         });
     }
 }
+
+Clipboard.propTypes = propTypes;
 
 export default Clipboard;
